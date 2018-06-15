465e4c68ade7a54aa1115c45f58b688cca8f67a0
Gopher-43 <gopher-43@users.noreply.github.com>
2018-06-15 17:32:21 +0300

    #1 - checking changeges array k[], string interp, for, k[] order

--- solvepoly/Program.cs
+++ solvepoly/Program.cs
@@ -10,39 +10,49 @@ namespace solvepoly
     {
         static void Main(string[] argv)
         {
-            short i = 0;
-            for (short j,argv[j]<)
-            foreach (string v in argv)
+            //short i = 0;
+            double[] k = new double[argv.Length+20];
+            bool flag1 = false;//
+            int j = 0;
+            for (int i=0; i<argv.Length;i++)
             {
-                if (v = "-v") // Verbose
+                if (argv[i] == "-v")
                 {
-                    
-                    double k1 = double.Parse(argv[0]);
-                    double k2 = double.Parse(argv[1]);
-                    double k3 = double.Parse(argv[2]);
+                    flag1 = true;
+                    //coef[j] = Double.Parse(argv[i+1]);
+                   
+                    continue;
+                }
+                k[j] = Double.Parse(argv[i]);
+                j++;
+            }
+            
+                if (flag1)//(v == "-v") // Verbose
+                {
                     Console.WriteLine("***Input * **");
-                    Console.WriteLine(" Polynom of degree 2 ");
-                    Console.WriteLine(" x ^ 2 - x - 1 = 0 ");
-                    Console.WriteLine(" ***Solution * ** ");
-                    Console.WriteLine(" Solving by quadratic formula: ");
-                    Console.WriteLine(" Discriminant: ");
-                    Console.WriteLine(" D = b ^ 2 - 4ac ");
-                    Console.WriteLine(" D = (" + k2 + ")^2-4*(" + k1 + ")*(" + k3 +")" + ")");
-                    Console.WriteLine(" Roots: ");
+                    Console.WriteLine("Polynom of degree 2 ");
+                    Console.WriteLine($"{k[0]}x^2 + ({k[1]})x + ({k[2]}) = 0 ");
+                    Console.WriteLine("*** Solution ***");
+                    Console.WriteLine("Solving by quadratic formula: ");
                     //discriminant
-                    double discr = Math.Pow(k2, 2)-4*k1*k2;
-                    
+                    double discr = Math.Pow(k[1], 2) - 4 * k[0] * k[1];
+
+                    Console.WriteLine("Discriminant: ");
+                    Console.WriteLine("D = b^2 - 4ac ");
+                    Console.WriteLine($"D = ({k[1]})^2-4*({k[0]})*({k[2]})");
+                    Console.WriteLine($"D = {discr}");
+                    Console.WriteLine("Roots: ");
 
                     // D > 0 -> real roots
                     if (discr>0)
                     {
                         //roots
-                        double x1 = ((-k2) + Math.Sqrt(discr)) / (2 * k1);
-                        double x2 = ((-k2) + Math.Sqrt(discr)) / (2 * k1);
-                        Console.WriteLine(" D > 0, must be 2 roots ");
-                        Console.WriteLine(" x1,2 = (-b±√D)/ (2a) ");
-                        Console.WriteLine(" x1 = ("+ (-k2) + "+ √" + discr + ") / (2" + k1 + " * " + k1 + ")");
-                        Console.WriteLine(" x2 = ("+ (-k2) + "- √" + discr + ") / (2" + k1 + " * " + k1 + ")");
+                        double x1 = ((-k[1]) + Math.Sqrt(discr)) / (2 * k[0]);
+                        double x2 = ((-k[1]) + Math.Sqrt(discr)) / (2 * k[0]);
+                        Console.WriteLine("D > 0, must be 2 roots ");
+                        Console.WriteLine("x1,2 = (-b±√D)/ (2a) ");
+                        Console.WriteLine($"x1 = ({-k[1]} + √{discr}) / (2{k[0]} * {k[2]})");
+                        Console.WriteLine($"x1 = ({-k[1]} - √{discr}) / (2{k[0]} * {k[2]})");
                         Console.WriteLine(" **Results * *** ");
                         Console.WriteLine(x1);
                         Console.WriteLine(x2);
@@ -52,50 +62,40 @@ namespace solvepoly
                     // D = 0 -> one root
                     else if (discr==0)
                     {
-                        double x1 = (-k2) / (2 * k1);
-                        Console.WriteLine(" D = 0, must be 1 root ");
-                        Console.WriteLine(" x = (-b)/(2a) ");
-                        Console.WriteLine(" x1 = ( -(" + (k2) + ") ) / (2" + k1 + " * " + k1 + ")");
-                        Console.WriteLine(" **Results * *** ");
+                        double x1 = (-k[1]) / (2 * k[0]);
+                        Console.WriteLine("D = 0, must be 1 root ");
+                        Console.WriteLine("x = (-b)/(2a) ");
+                        Console.WriteLine($"x1 = ( -({k[1]} ) ) / (2{k[0]}");
+                        Console.WriteLine("**Results**");
                         Console.WriteLine(x1);
                         
                     }
                     else// D < 0 -> complex roots
                     {
                         Console.WriteLine(" D < 0, must be 2 complex roots ");
-                        Console.WriteLine(" x1,2 = (-b±i√|D|)/ (2a) ");
-                        Console.WriteLine(" x1 = (" + (-k2) + "+ √|" + discr + "|) / (2" + k1 + " * " + k1 + ")");
-                        Console.WriteLine(" x2 = (" + (-k2) + "- √|" + discr + "|) / (2" + k1 + " * " + k1 + ")");
+                        Console.WriteLine(" x1,2 = (-b±i√|D|)/(2a) ");
+                        Console.WriteLine($"x1 = ({-k[1]} +√|{discr}|)/(2{k[0]}*{k[0]})");
+                        Console.WriteLine($"x2 = ({-k[1]} -√|{discr}|)/(2{k[0]}*{k[0]})");
                         Console.WriteLine(" **Results * *** ");
-                        Console.WriteLine(" x1 = (" + (-k2) + "+√|" + discr + "|)/" + 2*k1 + "");
-                        Console.WriteLine(" x2 = (" + (-k2) + "-√|" + discr + "|)/" + 2*k1 + "");
+                        Console.WriteLine($"x1 = ({-k[1]} +√|{discr}|)/{2*k[0]}");
+                        Console.WriteLine($"x2 = ({-k[1]} -√|{discr}|)/{2*k[0]}");
 
                         //Console.WriteLine(x1);
                         //Console.WriteLine(x2);
                     }
-                    Console.WriteLine("  ");
-                    Console.WriteLine("  ");
-                    Console.WriteLine("  ");
-
-                    /*
-                     */
-
-                    break;//exit foreach
+                    Console.WriteLine("======");
                 }
                 else
                 {
 
-                    double k1 = double.Parse(argv[0]);
-                    double k2 = double.Parse(argv[1]);
-                    double k3 = double.Parse(argv[3]);
                     //discriminant
-                    double discr = Math.Pow(k2, 2) - 4 * k1 * k2;
+                    double discr = Math.Pow(k[1], 2) - 4 * k[0] * k[1];
                     // D > 0 -> real roots
                     if (discr > 0)
                     {
                         //roots
-                        double x1 = ((-k2) + Math.Sqrt(discr)) / (2 * k1);
-                        double x2 = ((-k2) + Math.Sqrt(discr)) / (2 * k1);
+                        double x1 = ((-k[1]) + Math.Sqrt(discr)) / (2 * k[0]);
+                        double x2 = ((-k[1]) + Math.Sqrt(discr)) / (2 * k[0]);
                         Console.WriteLine(x1);
                         Console.WriteLine(x2);
 
@@ -103,44 +103,17 @@ namespace solvepoly
                     // D = 0 -> one root
                     else if (discr == 0)
                     {
-                        double x1 = (-k2) / (2 * k1);
+                        double x1 = (-k[1]) / (2 * k[0]);
                         Console.WriteLine(x1);
 
                     }
                     else// D < 0 -> complex roots
                     {
-                        Console.WriteLine(" x1 = (" + (-k2) + "+√|" + discr + "|)/" + 2 * k1 + "");
-                        Console.WriteLine(" x2 = (" + (-k2) + "-√|" + discr + "|)/" + 2 * k1 + "");
+                        Console.WriteLine($"x1 = ({-k[1]}+√|{discr}|)/2 * {k[0]}");
+                        Console.WriteLine($"x2 = ({-k[1]}-√|{discr}|)/2 * {k[0]}");
                     }
-
-                    break;//exit foreach
                 }
-            i++;
-            }
-            Console.ReadKey();
+         
         }
     }
 }
-/*
-Verbose
-> solvepoly -v 1 -1 -1
-*** Input ***
-Polynom of degree 2
-x^2-x-1=0
-*** Solution ***
-Solving by quadratic formula:
-Discriminant:
-    D = b^2-4ac
-    D = (-1)^2-4*(1)*(-1) = 5
-Roots:
-    D > 0, must be 2 roots
-    x1,2 = (-b±√D)/(2a)
-    x1 = (1+√5)/(2*1) = 1.6180
-    x2 = (1-√5)/(2*1) = -0.6180
-*** Results ***
-1.6180
--0.6180
-
-
-
- */
